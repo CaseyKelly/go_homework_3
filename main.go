2e2492afd7ba69e2da1a92f42b464a209bf8f8a8
Casey Kelly <casey.kelly@colorado.edu>
2016-06-23 23:33:55 -0400

    homework 3 solution

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
+
+func main() {
+	// Capture and parse flags from command line
+	employeeSerial := flag.String("serial", "0", "employee serial number")
+	filename := flag.String("filename", "0", "filename")
+	flag.Parse()
+
+	// Call function to fetch employee record
+	readFile(*filename, *employeeSerial)
+}
+
+type Employee struct {
+	Name    string
+	Serial  string
+	Age     string
+	Address string
+}
+
+// Function reads txt input and save it in a variable
+func readFile(filename string, employeeSerial string) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	str := string(data)
+
+	//	Splits up input into slice of student record strings
+	spltStr := strings.Split(str, "&")
+
+	// Initialize map of student records
+	mapOfRecords := make(map[string]Employee)
+
+	//	Loop through chunks and place them in struct for each employee
+	for i := 0; i < (len(spltStr)); i++ {
+		spltSlice := strings.Split(spltStr[i], ",")
+		studentStruct := Employee{
+			Name:    spltSlice[0],
+			Serial:  spltSlice[1],
+			Age:     spltSlice[2],
+			Address: spltSlice[3],
+		}
+		//	Place each struct into map where key is serial and map is record
+		mapOfRecords[spltSlice[1]] = studentStruct
+	}
+	// Prints out record of serial passed as flag from command line
+	fmt.Println("Name: " + mapOfRecords[employeeSerial].Name)
+	fmt.Println("Serial: " + mapOfRecords[employeeSerial].Serial)
+	fmt.Println("Age: " + mapOfRecords[employeeSerial].Age)
+	fmt.Println("Address: " + mapOfRecords[employeeSerial].Address)
+}
+
+// Questions:
+
+// Why one letter in all examples and not full word? Smaller file size?
+// Why do you have to parse flags from command line?
---
+++ readme.md
@@ -0,0 +1,20 @@
+Hi Gang,
+
+Here's the homework for next time:
+
+- Accept a filename and an employee serial number as command line input
+- Read list of employees from the file
+- Print out employee information
+
+
+Data input example:
+- One long line.  Each address separated by '&' character.
+- Each part of address separated by comma
+“Ann,598737,22,123 Main Street&Bob,746053,21,34 Grand Ave&Sally,847622,34,3901 South Miami Blvd
+
+Example execution
+myprog -filename myfile.txt -serial 746053
+Name: Bob
+Serial: 746053
+Age: 21
+Address: 34 Grand Ave
